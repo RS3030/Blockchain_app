80c771b40247c02206e85a37d8943ef257c0a058
RS3030 <skmsr32@gmail.com>
2021-06-29 12:57:45 +0900

    ?? store blockchain data to a file

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,8 @@
-from functools import reduce
+from hash_util import hash_block, hash_string_256
+import json
 from collections import OrderedDict
+from functools import reduce
 
-
-from .hash_util import hash_block, hash_string_256
 
 MINING_REWARD = 10
 
@@ -19,6 +19,42 @@ blockchain = [genesis_block]
 open_transactions = []
 owner = 'Ryo'
 participants = {'Ryo'}
+
+
+def load_data():
+    with open('blockchain.txt', mode='r') as f:
+        file_content = f.readlines()
+        global blockchain
+        global open_transactions
+        blockchain = json.loads(file_content[0][:-1])
+        updated_blockchain = []
+        for block in blockchain:
+            updated_block = {
+                'previous_hash': block['previous_hash'],
+                'index': block['index'],
+                'transactions': [OrderedDict(
+                    [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])]) for tx in block['transactions']],
+                'proof': block['proof']
+            }
+            updated_blockchain.append(updated_block)
+        blockchain = updated_blockchain
+        open_transactions = json.loads(file_content[1])
+        updated_transactions = []
+        for tx in open_transactions:
+            updated_tx = OrderedDict(
+                [('sender', tx['sender']), ('recipient', tx['recipient']), ('amount', tx['amount'])]),
+            updated_transactions.append(updated_tx)
+        open_transactions = updated_transactions
+
+
+load_data()
+
+
+def save_data():
+    with open('blockchain.txt', mode='w') as f:
+        f.write(json.dumps(blockchain))
+        f.write('\n')
+        f.write(json.dumps(open_transactions))
 
 
 def valid_proof(transactions, last_hash, proof):
@@ -80,6 +116,7 @@ def add_transaction(recipient, sender=owner, amount=1.0):
         open_transactions.append(transaction)
         participants.add(sender)
         participants.add(recipient)
+        save_data()
         return True
     return False
 
@@ -161,6 +198,7 @@ while waiting_for_input:
     elif user_choice == '2':
         if mine_block():
             open_transactions = []
+            save_data()
     elif user_choice == '3':
         print_blockchain_elements()
     elif user_choice == '4':
